7f6c9308184b677b1906ec3b1bfff28e8ffebc98
hugojesusmr <hugojesusmr@gmail.com>
2022-07-07 23:09:28 +0000

    creando una rama employee-job

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('employee-job') {
+    definition {
+        cpsScm{
+            scm {
+                git{
+                    remote{
+                        url 'https://github.com/hugojesusmr/employee.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
