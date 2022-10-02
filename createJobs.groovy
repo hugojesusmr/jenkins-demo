63bb94ffd945db714c4fa78248915ae1d850ee94
hugojesusmr <hugojesusmr@gmail.com>
2022-10-02 18:43:47 +0000

    c

--- createJobs.groovy
+++ createJobs.groovy
@@ -7,7 +7,9 @@ pipelineJob('employee-job-sonarqube') {
                         url 'https://github.com/hugojesusmr/microservicesjenkins.git'
                     }
                     branch 'master'
-                    scriptPath('Jenkinsfile')
+                    dir('config-server') {
+                    scriptPath('config-server/Jenkinsfile')
+                    }
                 }
             }
         }
