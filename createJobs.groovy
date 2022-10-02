2412f59ab2fbe9bb2c134f144c8eac4e1efbea43
hugojesusmr <hugojesusmr@gmail.com>
2022-10-02 21:41:29 +0000

    productservice

--- createJobs.groovy
+++ createJobs.groovy
@@ -13,6 +13,21 @@ pipelineJob('configservice') {
         }
     }
 }  
+pipelineJob('serviceproduct') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/hugojesusmr/microservicesjenkins.git'
+                    }
+                    branch 'productservice'
+                    scriptPath('product-service/Jenkinsfile')
+                }
+            }
+        }
+    }
+}  
 
 
 
